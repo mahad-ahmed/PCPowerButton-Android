32490164357b9ecca62fc1e1644c328b4995a878
mahad-ahmed <mahad628@gmail.com>
2019-03-31 13:46:06 +0500

    Removed unused import

--- app/src/main/java/com/atompunkapps/pcpowerbutton/MainActivity.java
+++ app/src/main/java/com/atompunkapps/pcpowerbutton/MainActivity.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckedTextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
